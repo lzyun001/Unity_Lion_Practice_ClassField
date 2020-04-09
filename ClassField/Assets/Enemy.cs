c9974f4b2a1bd6f92caf420f8ef8d8cdc317ef7b
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-04-09 10:21:01 +0800

    已完成

---
+++ ClassField/Assets/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("血量"),Tooltip("怪物的血量"),Range(50, 500)]
+    public int hp;
+
+    [Header("魔力"), Tooltip("怪物的魔力"), Range(10, 100)]
+    public int mp = 100;
+
+    [Header("能力值"), Range(1, 50.5f)]
+    public float ms = 10.5f;
+    [Range(0, 100)]
+    public int att = 50;
+    [Range(0, 100)]
+    public int def;
+
+    [Header("裝備")]
+    public string weapon;
+    public string equipment = "皮衣";
+
+    [Header("其他資料"), Tooltip("怪物是否帶有鑰匙")]
+    public bool key;
+    [Tooltip("怪物是否帶有寶物"),]
+    public bool treasure;
+}
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("怪物1號")]
+    public Enemy enemy1;
+    [Header("怪物2號")]
+    public Enemy enemy2;
+
+    private void Start()
+    {
+        print("怪物一號的魔力" + enemy1.mp);
+        print("怪物二號的裝備名稱" + enemy2.equipment);
+
+        enemy1.hp = 100;
+        enemy2.hp = 300;
+        enemy1.def = 50;
+        enemy1.weapon = "木棍";
+        enemy2.weapon = "短刀";
+        enemy1.key = true;
+        enemy2.treasure = true;
+    }
+}
